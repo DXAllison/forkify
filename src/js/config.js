437c322e688629941d1e54c7f3081f6322eb1127
D'Marcus Xavier <82902261+DXAllison@users.noreply.github.com>
2025-09-17 11:55:10 -0400

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7b3e3f71-1b51-4007-b0f7-487413757111';
+export const MODAL_CLOSE_SEC = 2.5; // 2.5 seconds
